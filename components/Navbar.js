a824bafe40740c2303122a57a2505dee318f3255
Filipe <filipefreire@protonmail.com>
2021-05-07 13:36:21 +0200

    Add netlify identity implementation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,42 @@
-import Link from 'next/link'
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
+import AuthContext from "../stores/authContext";
+import { useContext } from "react";
 
 export default function Navbar() {
+  const { user, login, logout } = useContext(AuthContext);
+
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
         <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/guides">
+              <a>Guides</a>
+            </Link>
+          </li>
+
+          {(!user && (
+            <li onClick={login} className="btn">
+              Login/Signup
+            </li>
+          )) || (
+            <li onClick={logout} className="btn">
+              Logout
+            </li>
+          )}
         </ul>
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
       </div>
     </div>
-  )
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
-import Navbar from '../components/Navbar'
-import '../styles/globals.css'
+import Navbar from "../components/Navbar";
+import { AuthContextProvider } from "../stores/authContext";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <Navbar />
-      <Component {...pageProps} />
+      <AuthContextProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </AuthContextProvider>
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
